f809238a9aefc1fc390bc9daa586a9101f53817f
Richard van Denderen <>
2018-07-16 16:08:24 +0200

    use default gutenberg styles

---
+++ snippets/default-block-style.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * https://wordpress.org/gutenberg/handbook/extensibility/theme-support/#default-block-styles
+ * adds default block styling to the front-end
+ */
+add_theme_support( 'wp-block-styles' );
