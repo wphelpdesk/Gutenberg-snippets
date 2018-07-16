1fbe9335b3f95da06679b87c74a0b01d05510b5b
Richard van Denderen <>
2018-07-16 16:08:35 +0200

    use new image alignment options

---
+++ snippets/image-align.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Use .alignwide and .alignfull in your CSS
+ *  https://wordpress.org/gutenberg/handbook/extensibility/theme-support/#wide-alignment
+ */
+add_theme_support( 'align-wide' );
+add_theme_support( 'align-full' );
